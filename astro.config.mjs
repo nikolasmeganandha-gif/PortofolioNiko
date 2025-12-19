3b63c291519f167962e31058bc4d21a481e989e4
nikolasmeganandha-gif <nikolasmeganandha@gmail.com>
2025-12-19 21:30:29 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	// site: 'https://example.com',
+	// integrations: [mdx(), sitemap()],
+	output: 'static',
+	site: 'https://nikolasmeganandha-gif.github.io',
+	base: '/myblog',
 });
